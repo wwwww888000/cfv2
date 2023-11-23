376c4102c871f496aaabeab5f03b758b7b6adce5
wwwww888000 <151737116+wwwww888000@users.noreply.github.com>
2023-11-23 19:58:02 +0800

    Update worker-vless.js

--- worker-vless.js
+++ worker-vless.js
@@ -5,9 +5,13 @@ import { connect } from 'cloudflare:sockets';
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
-
+// 输入自定义UUID
 let proxyIP = '';
-
+// cdn-all.xn--b6gac.eu.org 
+// cdn.xn--b6gac.eu.org 
+// cdn-b100.xn--b6gac.eu.org 
+// edgetunnel.anycast.eu.org 
+// cdn.anycast.eu.org
 
 if (!isValidUUID(userID)) {
 	throw new Error('uuid is not valid');
